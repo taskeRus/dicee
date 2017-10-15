1e6c50d69dfae1b8633a2b24be6d96d3202f88ed
Alexey <lolokovalex@mail.ru>
2017-10-15 03:33:28 +0300

    Updates.

--- dicee/ViewController.swift
+++ dicee/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var diceRandomIndex1: Int = 0
+    var diceRandomIndex2: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceRandomIndex1 = Int(arc4random_uniform(6))
+        diceRandomIndex2 = Int(arc4random_uniform(6))
+        
+    }
+    
 }
 
